09be69f935c7d429a5827704515dffc8435f219b
oliver <oliver@clarusway.com>
2022-04-24 13:26:05 +0300

    project203 modified

--- projects/203-dockerization-bookstore-api-on-python-flask-mysql/solution_files/main.tf
+++ projects/203-dockerization-bookstore-api-on-python-flask-mysql/solution_files/main.tf
@@ -51,7 +51,7 @@ resource "aws_instance" "tf-docker-ec2" {
   ami = "ami-0f9fc25dd2506cf6d"
   instance_type = "t2.micro"
   key_name = "oliver"
-  security_groups = [aws_security_group.tf-docker-sec-gr-203.id]
+  security_groups = ["docker-sec-group-203"]
   tags = {
     Name = "Web Server of Bookstore"
   }
