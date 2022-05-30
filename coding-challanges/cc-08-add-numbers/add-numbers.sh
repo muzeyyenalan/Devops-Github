0f97bac18549763187137dbb34b5b20c806f7cf0
tom <tyler@clarusway.com>
2022-05-30 03:04:22 -0400

    added CC-08

---
+++ coding-challanges/cc-08-add-numbers/add-numbers.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+if [ $# -ne 2 ]
+then
+echo "Usage - $0 x y"
+echo " Where x and y are two nos for which I will print sum"
+exit 1
+fi
+echo " Where x and y are two nos for which I will print sum"
+exit 1
+fi
+echo "Sum of $1 and $2 is `expr $1 + $2`"
